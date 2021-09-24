4187eda1b82d7f586c2190331ce6cccd487332b9
hirobe-junki <hirobe-junki.develop@promet-system.com>
2021-09-24 15:37:58 +0900

    mr test

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ const Home = ({ allPostsData }) => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p data-index-a>President of Promet System</p>
+        <p data-index-a>President of Promet System Develop</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
